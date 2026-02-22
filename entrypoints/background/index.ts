06ed03776104e5d50b14189769d8709e73841d36
p-o-f <parthib.faruk@gmail.com>
2026-02-22 06:03:30 -0700

    gemini wont work, need to feed it atranscript, it seems all inference IS INDEED local

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -1,6 +1,9 @@
 import { checkSystemResources } from "@/utils/resourcecheck";
 import { KeepAliveService } from "@/utils/keepalive";
 import { settingsStorage } from "@/utils/storage";
+
+import { YoutubeTranscript } from 'youtube-transcript';
+
 export default defineBackground(async () => {
   console.log("Hello background!", { id: browser.runtime.id });
   KeepAliveService.start();
@@ -25,26 +28,29 @@ export default defineBackground(async () => {
   });
 
   // // Proceed to request batch or streaming summarization
-  const summarizer = await Summarizer.create({
-    monitor(m) {
-      m.addEventListener("downloadprogress", (e) => {
-        console.log(`Downloaded ${e.loaded * 100}%`);
-      });
-    },
-  });
+  // const summarizer = await Summarizer.create({
+  //   monitor(m) {
+  //     m.addEventListener("downloadprogress", (e) => {
+  //       console.log(`Downloaded ${e.loaded * 100}%`);
+  //     });
+  //   },
+  // });
 
-  console.log("Availability:", availability);
-  console.log("trying yolo to cloud sumamry toytoubeu");
-  const dummyText2 = "https://www.youtube.com/watch?v=httnhdpu_W4";
-  const summary2 = await summarizer.summarize(dummyText2, {
-    context: "What is the title of this video and a short summary?",
-  });
-  console.log(summary2);
+  // console.log("Availability:", availability);
+  // console.log("trying yolo to cloud sumamry toytoubeu");
+  // const dummyText2 = "https://www.youtube.com/watch?v=httnhdpu_W4";
+  // const summary2 = await summarizer.summarize(dummyText2, {
+  //   context: "What is the title of this video and a short summary?",
+  // });
+  // console.log(summary2);
   console.log("completed!!!");
   // console.log("trying yolo to cloud sumamry toytoubeu");
   // const dummyText2 = "https://youtu.be/httnhdpu_W4";
   // console.log(await summarizeUrl(dummyText2, "key-points", "medium", "give me the title of the video"));
   // console.log("completed!!!");
+  transcribe();
+  YoutubeTranscript.fetchTranscript('-q2n5DkDoMQ').then(console.log);
+
 });
 
 // todo https://developer.chrome.com/docs/ai/scale-summarization and chrome://on-device-internals/ <-- just reference this for docs l8r
@@ -65,3 +71,22 @@ const result = await summarizer.summarize(url, {
 summarizer.destroy(); // free up resources
 return result;
 }
+
+function transcribe() {
+//   (async () => {
+//   try {
+//     const transcript = await YoutubeTranscript.fetchTranscript("I7_WXKhyGms");
+//     console.log(transcript);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+YoutubeTranscript.fetchTranscript('EpSmQa7UyA0').then(console.log);
+
+}
+
+// FIX FOR LATER
+// TODO
+
+// https://github.com/Kakulukian/youtube-transcript/issues/45#issuecomment-2906780325 <--------
+
